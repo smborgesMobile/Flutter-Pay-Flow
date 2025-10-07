261ca9996ee4c165f4aff80f6e2e72eac597f477
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-10-07 15:18:21 -0300

    feat: create hoem bottom navigation

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:learning_project/shared/themes/app_colors.dart';
+import 'package:learning_project/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Home Page")));
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: TextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                      text: "Gabul",
+                      style: TextStyles.titleBoldBackground,
+                    ),
+                  ],
+                ),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                width: 48,
+                height: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: const Center(child: Text("Conteúdo principal aqui")),
+      bottomNavigationBar: SafeArea(
+        top: false,
+        child: SizedBox(
+          height: 90,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+                color: AppColors.primary,
+              ),
+              GestureDetector(
+                onTap: () {
+                  print("Clicou");
+                },
+                child: Container(
+                  width: 56,
+                  height: 56,
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Icon(Icons.add_outlined, color: AppColors.background),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.description_outlined),
+                color: AppColors.body,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
