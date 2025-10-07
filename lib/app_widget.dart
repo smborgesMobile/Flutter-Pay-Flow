b2853aaaedbc436a8129952ffa47d088524f8183
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-10-07 11:43:47 -0300

    feat: setup firebase

---
+++ lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:learning_project/modules/login/login_page.dart';
+import 'package:learning_project/shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Pay Flow",
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: LoginPage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:learning_project/modules/login/login_page.dart';
-import 'package:learning_project/shared/themes/app_colors.dart';
+import 'package:learning_project/firebase_widget.dart';
 
 void main() {
-  runApp(AppWidget());
-}
-
-class AppWidget extends StatelessWidget {
-  const AppWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Pay Flow",
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: LoginPage()
-    );
-  }
+  runApp(AppFirebase());
 }
