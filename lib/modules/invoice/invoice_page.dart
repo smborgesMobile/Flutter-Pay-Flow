2d4b51b076e0628330d51762ab4c8f2db1a1c2ce
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-10-09 12:13:15 -0300

    feat: add boletos scren

--- lib/modules/invoice/invoice_page.dart
+++ lib/modules/invoice/invoice_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:learning_project/shared/themes/app_colors.dart';
 import 'package:learning_project/shared/themes/app_text_styles.dart';
 import 'package:learning_project/shared/widgets/bar_code_counter/bar_code_counter.dart';
-import 'package:learning_project/shared/widgets/invoice_item/invoice_item_widget.dart';
+import 'package:learning_project/shared/widgets/invoid_list/invoice_list_widget.dart';
 import '../../shared/models/invoice_model.dart' show Invoice;
 
 class InvoicePage extends StatefulWidget {
@@ -48,50 +48,35 @@ class InvoicePage extends StatefulWidget {
 class _InvoicePageState extends State<InvoicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  color: AppColors.primary,
-                  height: 80,
-                  width: double.maxFinite,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: BarCodeCounter(counter: 1),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text("Meus boletos", style: TextStyles.titleBoldHeading),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                color: AppColors.primary,
+                height: 80,
+                width: double.maxFinite,
               ),
+              Padding(
+                padding: const EdgeInsets.all(24),
+                child: BarCodeCounter(counter: 1),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text("Meus boletos", style: TextStyles.titleBoldHeading),
             ),
-            Padding(
-              padding: const EdgeInsets.all(24),
-              child: Divider(height: 1, color: AppColors.stroke, thickness: 1),
-            ),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: widget.invoices.length,
-              itemBuilder: (context, index) {
-                final invoice = widget.invoices[index];
-                return InvoiceItemWidget(
-                  name: invoice.name,
-                  expiredData: invoice.expiredData,
-                  currency: invoice.currency,
-                );
-              },
-            ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Divider(height: 1, color: AppColors.stroke, thickness: 1),
+          ),
+         InvoiceListWidget()
+        ],
       ),
     );
   }
